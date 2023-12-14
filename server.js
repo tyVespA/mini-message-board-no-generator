18071bbe8245b8b8a148e254fa9b022819bd300d
tyVespA <migliavacca.marco08@gmail.com>
2023-12-14 23:36:21 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { text: "Testing" });
+});
+
+app.listen(3000);

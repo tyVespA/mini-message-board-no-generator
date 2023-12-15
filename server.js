38111668a051b4940a36e2a390e4144a1aa18425
tyVespA <migliavacca.marco08@gmail.com>
2023-12-15 23:44:00 +0100

    Add route

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
+const router = require("./routes/index");
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index", { text: "Testing" });
-});
+app.use("/", router);
+
+// app.get("/", (req, res) => {
+//   res.render("index", { title: "Mini Messageboard", messages: messages });
+// });
 
 app.listen(3000);

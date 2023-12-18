f60590357d06865c88fc16948f4dfde29be3515b
tyVespA <migliavacca.marco08@gmail.com>
2023-12-18 12:38:18 +0100

    Add port variable

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 
 const router = require("./routes/index");
 
+const port = process.env.PORT || 3000;
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/", router);
 
-app.listen(3000, "0.0.0.0");
+app.listen(port);
